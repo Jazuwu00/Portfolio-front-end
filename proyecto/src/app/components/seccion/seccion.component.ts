282017bb22a06f437aaad6c28edd390079112391
jazmin <jsaraviaa98@gmail.com>
2023-01-27 20:20:03 -0300

    empezando con angular

---
+++ proyecto/src/app/components/seccion/seccion.component.ts
@@ -0,0 +1,10 @@
+import { Component,OnInit,Input,Output} from '@angular/core';
+
+@Component({
+  selector: 'app-seccion',
+  templateUrl: './seccion.component.html',
+  styleUrls: ['./seccion.component.css']
+})
+export class SeccionComponent {
+
+}
