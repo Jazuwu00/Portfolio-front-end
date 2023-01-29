8102f7351c14c053b3c977fe5f2712a44d61cd12
jazmin <jsaraviaa98@gmail.com>
2023-01-29 19:54:26 -0300

    uwu

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SeccionComponent } from './components/seccion/seccion.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SeccionComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
